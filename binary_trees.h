07f9da88424144da71cdc231e9877ce6ddb3a68d
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-04-07 18:52:02 +0000

    feat: implement balance factor calculation

--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,4 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-Where node is a pointer to the node to find the uncle
 #endif /* _BINARY_TREES_H_ */
